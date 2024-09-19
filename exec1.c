72eda542568c5303c6bc7384efd742349e409f43
H7Pradi <120180747+H7Pradi@users.noreply.github.com>
2024-09-19 08:33:02 +0000

    Add files via upload

---
+++ exec1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+int main()
+{
+	char* cmnd="ls";
+	char* argument[]={"ls","-1",NULL};
+	printf("Before execvp()\n");
+	pid_t p=fork();
+	if(p==0)
+	{
+		printf("Child process\n");
+		int status= execvp(cmnd,argument);
+		if(status=-1)
+	{
+		printf("Terminated\n");
+	}
+	else
+	{
+		printf("Parents process\n");
+		wait(NULL);  	
+		printf("Done\n");
+	}
+}
+}
